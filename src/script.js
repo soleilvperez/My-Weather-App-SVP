268b4f02f1f292a1268d183045e35d33296fee12
Soleil P <soleilvperez@gmail.com>
2021-10-28 22:47:51 -0400

    Removed unit conversion

--- src/script.js
+++ src/script.js
@@ -1,48 +1,3 @@
-function getForecast(coordinates) {
-  let apiKey = "667d9f573c8af4c33457be5d561a9148";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
-
-  console.log(apiUrl);
-  axios.get(apiUrl).then(displayForecast);
-}
-
-function cityTemp(response) {
-  fahrenheitTemperature = response.data.main.temp;
-
-  document.querySelector("#current-city").innerHTML = `📍${response.data.name}`;
-  document.querySelector("#location-temperature").innerHTML = Math.round(
-    fahrenheitTemperature
-  );
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-
-  document.querySelector("#weather-description").innerHTML =
-    response.data.weather[0].main;
-
-  let iconElement = document.querySelector("#icon");
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-
-  getForecast(response.data.coord);
-}
-
-function searchCity(city) {
-  let apiKey = "667d9f573c8af4c33457be5d561a9148";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-
-  axios.get(apiUrl).then(cityTemp);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-  searchCity(city);
-}
-
 function formatDate(date) {
   let hours = date.getHours();
   if (hours < 10) {
@@ -122,41 +77,56 @@ function displayForecast(response) {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
+function getForecast(coordinates) {
+  let apiKey = "667d9f573c8af4c33457be5d561a9148";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function cityTemp(response) {
+  fahrenheitTemperature = response.data.main.temp;
+
+  document.querySelector("#current-city").innerHTML = `📍${response.data.name}`;
+  document.querySelector("#location-temperature").innerHTML = Math.round(
+    fahrenheitTemperature
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].main;
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+
+  getForecast(response.data.coord);
+}
+
+function searchCity(city) {
+  let apiKey = "667d9f573c8af4c33457be5d561a9148";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(cityTemp);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
 
 let timeElement = document.querySelector("#time-display");
 let timedate = new Date();
 
 timeElement.innerHTML = formatDate(timedate);
-
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  let cityTemp = document.querySelector("#location-temperature");
-
-  fahrenheitLink.classList.remove("active");
-  celsiusLink.classList.add("active");
-
-  let celsiusTemperature = (fahrenheitTemperature - 32) * (5 / 9);
-
-  cityTemp.innerHTML = Math.round(celsiusTemperature);
-}
-
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  fahrenheitLink.classList.add("active");
-  celsiusLink.classList.remove("active");
-  let cityTemp = document.querySelector("#location-temperature");
-  cityTemp.innerHTML = Math.round(fahrenheitTemperature);
-}
-
-let fahrenheitTemperature = null;
-
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", handleSubmit);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 searchCity("Honolulu");
